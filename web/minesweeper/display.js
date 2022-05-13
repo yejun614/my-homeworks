7a4e4df8c5a34e4507bf4722366cb1a1891fdb5c
YeJun, Jung <yejunjung98@gmail.com>
2022-05-13 10:02:19 +0900

    Feat: change a image for reset button

--- web/minesweeper/display.js
+++ web/minesweeper/display.js
@@ -1,12 +1,31 @@
 
 class GameDisplay {
-  constructor (game, mineCounter, playTime) {
+
+  images = {
+    'idle': './smile-idle.png',
+    'cool': './smile-cool.png',
+    'over': './smile-over.png',
+    'click': './smile-click.png',
+  };
+
+  defaultImage = 'idle'
+
+  constructor (game, resetBtn, mineCounter, playTime) {
     this.game = game;
+    this.resetBtn = resetBtn;
+    this.resetBtnImage = this.resetBtn.querySelector('img');
     this.mineCounter = mineCounter;
     this.playTime = playTime;
 
+    this.addEvents();
     this.display();
     this.update();
+  }
+
+  addEvents() {
+    this.resetBtn.addEventListener('click', () => this.game.reset());
+    this.game.canvas.addEventListener('mousedown', () => this.changeResetButton('click'));
+    this.game.canvas.addEventListener('mouseup', () => this.changeResetButton('idle'));
   }
 
   padLeft(number, digits) {
@@ -21,9 +40,25 @@ class GameDisplay {
     return numStr;
   }
 
+  changeResetButton(image = '') {
+    if (image != '') {
+      this.defaultImage = image;
+
+    } else if (this.game.isGameOver) {
+      this.resetBtnImage.src = this.images['over'];
+
+    } else if (this.game.isGameClear) {
+      this.resetBtnImage.src = this.images['cool'];
+
+    } else {
+      this.resetBtnImage.src = this.images[this.defaultImage];
+    }
+  }
+
   display() {
     this.mineCounter.innerText = this.padLeft(this.game.mineCounter <= 0 ? 0 : this.game.mineCounter, 3);
     this.playTime.innerText = this.padLeft(this.game.playTime, 3);
+    this.changeResetButton();
   }
 
   update(timestamp) {
--- web/minesweeper/script.js
+++ web/minesweeper/script.js
@@ -2,14 +2,10 @@
 const gameCanvas = document.querySelector('#game-canvas');
 const mineCounter = document.querySelector('#mine-counter');
 const playTime = document.querySelector('#play-time');
-
-const game = new MinesweeperGame(gameCanvas, 9, 9, 10, 20);
-const gameDisplay = new GameDisplay(game, mineCounter, playTime);
-
 const resetBtn = document.querySelector('#reset-btn');
-resetBtn.addEventListener('click', () => {
-  game.reset();
-});
+
+const game = new MinesweeperGame(gameCanvas, 20, 20, 99, 20);
+const gameDisplay = new GameDisplay(game, resetBtn, mineCounter, playTime);
 
 const dashboard = document.querySelector('#dashboard');
 dashboard.style.width = `${gameCanvas.width}px`;
